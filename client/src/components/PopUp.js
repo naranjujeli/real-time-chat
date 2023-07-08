33c713c09457c2ab89bfe315e1023ad03562850d
naranjujeli <lucadive@gmail.com>
2023-07-08 15:47:19 -0300

    Allow PopUp to also be closed with enter

--- client/src/components/PopUp.js
+++ client/src/components/PopUp.js
@@ -20,6 +20,7 @@ export default function PopUp({ message, answer, setAnswer, handleConfirm }) {
         <TextInput
         content={answer}
         setContent={setAnswer}
+        action={handleConfirm}
         />
         <button onClick={() => {if (checkEmpty()) handleConfirm();}}>
           OK
